853993a92b2ed6ac43cb1143a4ce109953be3bbb
= <joseph-50@live.com>
2014-07-07 23:30:15 +0800

    Changing Create Event

--- src/com/example/CommunityOutreach/util/Settings.java
+++ src/com/example/CommunityOutreach/util/Settings.java
@@ -1,9 +1,17 @@
 package com.example.CommunityOutreach.util;
 
+import java.text.SimpleDateFormat;
+
 public interface Settings {
 	final static String DB_USER_NAME = "root";
 	final static String DB_PASSWORD = "mysql";
 	final static String DB_IP_ADDRESS = "localhost";
 	final static String DB_PORT = "3306";
 	final static String DB_DATABASE_NAME = "community_outreach";
+	
+	SimpleDateFormat dateFormatter = new SimpleDateFormat("dd MMM yyyy");
+	SimpleDateFormat timeFormatter = new SimpleDateFormat("HH:mm a");
+	SimpleDateFormat dateTimeFormatter = new SimpleDateFormat("dd MMM yyyy h:mm:ss a");
+	//E.g. Jul 27, 2014 9:00:00 AM
+	SimpleDateFormat sqlDateTimeFormatter = new SimpleDateFormat("MMM dd, yyyy hh:mm:ss aa");
 }
