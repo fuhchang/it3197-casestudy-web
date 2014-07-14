99035f1da575d128c09aee50f70f6d98a9ba3a4d
Keith <Keith@Keith-PC>
2014-07-14 15:07:39 +0800

    Updated

--- src/com/example/CommunityOutreach/servlet/ArticleSubmissionServlet.java
+++ src/com/example/CommunityOutreach/servlet/ArticleSubmissionServlet.java
@@ -1,6 +1,7 @@
 package com.example.CommunityOutreach.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -44,6 +45,17 @@ public class ArticleSubmissionServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter out = response.getWriter();
+        response.setContentType("text/html");
+        response.setHeader("Cache-control", "no-cache, no-store");
+        response.setHeader("Pragma", "no-cache");
+        response.setHeader("Expires", "-1");
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "GET,POST");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type");
+        response.setHeader("Access-Control-Max-Age", "86400");
+		
 		// TODO Auto-generated method stub
 		String title = request.getParameter("title");
 		String category = (request.getParameter("category"));
