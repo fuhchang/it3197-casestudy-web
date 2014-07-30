25e9bc8177cf754c6c6e2c4a63a850f70d8783e3
fuhchang <fuhchang123@gmail.com>
2014-07-30 11:23:35 +0800

    upload image servlet

--- src/com/example/CommunityOutreach/servlet/cs/UploadImageServlet.java
+++ src/com/example/CommunityOutreach/servlet/cs/UploadImageServlet.java
@@ -1,5 +1,6 @@
 package com.example.CommunityOutreach.servlet.cs;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -8,6 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 
 /**
  * Servlet implementation class UploadImageServlet
@@ -15,7 +17,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/UploadImageServlet")
 public class UploadImageServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	private static String SAVE_DIR = "uploadFiles";   
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -36,8 +38,21 @@ public class UploadImageServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		InputStream in = request.getPart("file").getInputStream(); 
+		String appPath = request.getServletContext().getRealPath("");
+		String savePath = appPath + File.separator + SAVE_DIR; 
 		
+		File fileSaveDir = new File(savePath);
+		if (!fileSaveDir.exists()) {
+			fileSaveDir.mkdir();
+		}
+		String fileName = null;
+		for (Part part : request.getParts()) {
+			fileName = "haha.jpg";
+			System.out.println(savePath + File.separator + fileName);
+			part.write(savePath + File.separator + fileName);
+			
+			System.out.println(savePath + File.separator + fileName);
+		}
 	}
 
 }
