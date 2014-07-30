26846911f929ca23fd1046683b6aa5306b25b9a8
Sherry <sherrylau94@gmail.com>
2014-07-30 12:37:42 +0800

    Update

--- src/com/example/CommunityOutreach/servlet/InsertChoiceWebServlet.java
+++ src/com/example/CommunityOutreach/servlet/InsertChoiceWebServlet.java
@@ -1,6 +1,7 @@
 package com.example.CommunityOutreach.servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -14,6 +15,7 @@ import com.example.CommunityOutreach.data.RiddleManager;
 import com.example.CommunityOutreach.data.UserManager;
 import com.example.CommunityOutreach.model.Riddle;
 import com.example.CommunityOutreach.model.RiddleAnswer;
+import com.example.CommunityOutreach.model.RiddleUserAnswered;
 import com.example.CommunityOutreach.model.User;
 
 /**
@@ -65,15 +67,58 @@ public class InsertChoiceWebServlet extends HttpServlet {
 			 int riddleID = Integer.parseInt(request.getParameter("riddleID"));
 			 int riddleAnswerID = Integer.parseInt(request.getParameter("riddleAnswerID"));
 			 
-			 Riddle riddle = riddleManager.retrieveRiddle(riddleID);
-			 
 			 boolean result = riddleManager.insertChoice(riddleID, riddleAnswerID, nric);
 			 
 			 if(result) {
 				 RiddleAnswer riddleAns = riddleManager.retrieveRiddleAnswer(riddleAnswerID);
 				 
-				 if(riddleAns.getRiddleAnswerStatus().equals("CORRECT"))
+				 if(riddleAns.getRiddleAnswerStatus().equals("CORRECT")) {
+					 /* Calculation for difficulty level */
+					 ArrayList<RiddleUserAnswered> answeredList = riddleManager.retrieveAllAnswered(riddleID);
+					 ArrayList<RiddleAnswer> riddleAnsList = riddleManager.retrieveRiddleAnswers(riddleID);
+					 
+					 int correctlyAnswered = 0, easyRate = 0, mediumRate = 0, hardRate = 0, totalRate = 0;
+					 for(int i = 0; i < answeredList.size(); i++) {
+						 for(int ii = 0; ii < riddleAnsList.size(); ii++) {
+							 if(answeredList.get(i).getRiddleAnswer().getRiddleAnswerID() == riddleAnsList.get(ii).getRiddleAnswerID()) {
+								 if(riddleAnsList.get(ii).getRiddleAnswerStatus().equals("CORRECT")) {
+									 correctlyAnswered++;
+								 }
+							 }
+						 }
+						 if(answeredList.get(i).getAnsweredRate().equals("EASY")) {
+							 easyRate++;
+							 totalRate++;
+						 }
+						 else if(answeredList.get(i).getAnsweredRate().equals("MEDIUM")) {
+							 mediumRate++;
+							 totalRate++;
+						 }
+						 else if(answeredList.get(i).getAnsweredRate().equals("HARD")) {
+							 hardRate++;
+							 totalRate++;
+						 }
+					 }
+					 
+					 double correctAnsPercentage, ratePercentage, finalPercentage;
+					 correctAnsPercentage = (correctlyAnswered / answeredList.size()) * 100;
+					 ratePercentage = (easyRate * 0.2) + (mediumRate * 0.5) + (hardRate * 0.8) / totalRate;
+					 finalPercentage = correctAnsPercentage * ratePercentage;
+					 
+					 Riddle riddle = riddleManager.retrieveRiddle(riddleID);
+					 if(0 < finalPercentage && finalPercentage < 0.33){
+						 riddle.setRiddlePoint(5);
+					 }
+					 else if(0.34 < finalPercentage && finalPercentage < 0.66){
+						 riddle.setRiddlePoint(10);
+					 }
+					 else if(0.67 < finalPercentage && finalPercentage < 1){
+						 riddle.setRiddlePoint(20);
+					 }
+					 /* End of calculation for difficulty level */
+					 
 					 userManager.updatePoints(nric, user.getPoints()+riddle.getRiddlePoint());
+				 }
 			 }
 		 }
 		 else if(request.getParameter("rating") != null) {
